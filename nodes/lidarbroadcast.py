a8dd7b4a4b4d34a4e1dd29d30e2d8334aab2c06e
xaxxon <colin@xaxxon.com>
2019-11-07 19:29:38 -0800

    version 1.1, debug output cleanup

--- nodes/lidarbroadcast.py
+++ nodes/lidarbroadcast.py
@@ -19,8 +19,8 @@ dropscan_degpersec = 0
 parkoffset = 0 # used only by device firmware, set by cfg
 
 DIR = 1 # motor direction (1=CW/RHR+ with motor @ bottom, 0=CCW, ROS default)
-debugoutput = True
-VERSION = "1.0"
+DEBUGOUTPUT = False
+VERSION = "1.1"
 
 dropScanTurnRateThreshold = 0 # 0.785 radians, 0 = disabled
 lastodomth = None
@@ -32,10 +32,20 @@ turnrate = 0 # radians per second
 def cleanup():
 	global ser
 	ser.write("f\n") # stop lidar
+	
+	if DEBUGOUTPUT:
+		rospy.sleep(0.5)
+		ser.reset_input_buffer()
+		ser.write("z\n") # get i2c_error count
+		rospy.sleep(0.1)
+		while ser.inWaiting() > 0:
+			line = "SCAN: "+ser.readline().strip()
+			rospy.loginfo(line)
+		
 	ser.close()
-	removelockfiles()
 	rospy.sleep(3)
-	print("lidar disabled, shutdown");
+	rospy.loginfo("SCAN: lidar disabled, shutdown");
+	
 	
 def odomCallback(data):
 	global turning, turnrate, lastodomth, lastodomtime
@@ -64,11 +74,12 @@ def odomCallback(data):
 	lastodomth = odomth
 	lastodomtime = now
 	
+	
 def dynamicConfigCallback(config, level):
 	global newheaderoffset, minimum_range, maximum_range
 	global dropScanTurnRateThreshold, distortscan
 	
-	if debugoutput:
+	if DEBUGOUTPUT:
 		print(config)
 		
 	updateMasks(config.masks)
@@ -87,6 +98,7 @@ def dynamicConfigCallback(config, level):
 	updateReadInterval(config.read_frequency)
 
 	return config
+	
 	
 def updateHeaderOffset(offset):
 	global headeroffset
@@ -119,6 +131,7 @@ def updateHeaderOffset(offset):
 		
 	# client.update_configuration({"header_offset":headeroffset})
 
+
 def updateMasks(string):
 	global masks
 	
@@ -137,6 +150,7 @@ def updateMasks(string):
 		print("uneven number of masks")
 		masks=[]
 
+
 def updateRPM(speed):
 	global rpm
 
@@ -146,6 +160,7 @@ def updateRPM(speed):
 	rpm = speed
 	print("sending rpm to device: "+str(rpm))
 	ser.write("r"+chr(rpm)+"\n")  
+	
 	
 def updateParkOffset(offset):
 	global parkoffset
@@ -163,6 +178,7 @@ def updateParkOffset(offset):
 	ser.write(chr(val2))
 	ser.write("\n")
 	
+	
 def updateReadInterval(frequency):
 	global readInterval
 	
@@ -171,7 +187,6 @@ def updateReadInterval(frequency):
 		return
 		
 	readInterval = interval/1000000.0	
-	print("readInterval"+str(readInterval))
 	print("sending read interval to device: "+str(interval))
 	val1 = interval & 0xFF
 	val2 = (interval >>8) & 0xFF
@@ -179,6 +194,164 @@ def updateReadInterval(frequency):
 	ser.write(chr(val1))
 	ser.write(chr(val2))
 	ser.write("\n")
+	
+	
+def readlidar(ser):
+	
+	ser.write("y\n") # get version
+	rospy.sleep(0.1)
+	while ser.inWaiting() > 0:
+		line = ser.readline().strip()
+		print(line)
+		
+	ser.write("r"+chr(rpm)+"\n")  
+	ser.write("d"+chr(DIR)+"\n")  # set direction (1=CW RHR+ motor@bottom default, ROS default)
+	ser.write("a\n") # start lidar
+
+	# clear buffer
+	ser.reset_input_buffer()
+
+	raw_data = []
+	lastscan = rospy.Time.now()
+	headercodesize = 4
+	current_time = 0
+	dropscan = False
+	scannum = 0
+	lastcount = 0
+
+	while not rospy.is_shutdown() and ser.is_open:
+
+		# read data and dump into array, checking for header code 0xFF,0xFF,0xFF,0xFF
+		ch = ser.read(1)  
+		
+		# if timed out after TIMEOUT seconds, defined in usbdiscover.py)
+		if len(ch) == 0:
+			rospy.logerr("no response from xaxxonlidar device")
+			break
+		
+		raw_data.append(ch)
+		
+		if turning:
+			dropscan = True
+				
+		if not ord(ch) == 0xFF:
+			continue
+		else:
+			ch = ser.read(1)
+			raw_data.append(ch)
+			if not ord(ch) == 0xFF:
+				continue
+			else: 
+				ch = ser.read(1)
+				raw_data.append(ch)
+				if not ord(ch) == 0xFF:
+					continue
+				else: 
+					ch = ser.read(1)
+					raw_data.append(ch)
+					if not ord(ch) == 0xFF:
+						continue
+
+		ser.write("h\n") # send host hearbeat (every <10 sec minimum)
+
+		# read count		
+		low = ord(ser.read(1))
+		high = ord(ser.read(1))
+		count = (high<<8)|low
+		
+		""" read cycle """
+		c1 = ord(ser.read(1))
+		c2 = ord(ser.read(1))
+		c3 = ord(ser.read(1))
+		c4 = ord(ser.read(1))
+		cycle = ((c4<<24)|(c3<<16)|(c2<<8)|c1)/1000000.0
+
+		""" host time """
+		current_time = rospy.Time.now() # - rospy.Duration(0.0) #0.015
+		rospycycle = current_time - lastscan
+		# cycle = rospycycle.to_sec()
+		lastscan = current_time	
+		
+		rospycount = (len(raw_data)-headercodesize)/2
+
+
+		# debug info
+		if scannum % 10 == 0 and DEBUGOUTPUT:
+			msg = "SCAN "+str(scannum)+": cycle: "+str(cycle)+", count: "+str(count)
+			rospy.loginfo(msg)
+		
+		if not rospycount == count and DEBUGOUTPUT:
+			msg = "SCAN "+str(scannum)+": count/data mismatch: "+ str( rospycount-count )
+			rospy.logerr(msg)
+
+		if count<5 and scannum > 0 and DEBUGOUTPUT:  
+			msg = "SCAN "+str(scannum)+": low data count: "+str(count)+", cycle: "+str(cycle)
+			msg += ", scan dropped"
+			rospy.logerr(msg)
+			del raw_data[:]
+			continue
+		
+		if abs(count - lastcount) > 2 and scannum > 3 and DEBUGOUTPUT:
+			msg = "SCAN "+str(scannum)+": count change from: "+ str(lastcount)+", to: "+str(count)
+			rospy.logerr(msg)
+		
+		lastcount = count
+		
+		
+		scannum += 1	
+		if scannum <= 3: # drop 1st few scans while lidar spins up
+			del raw_data[:]
+			continue
+		
+		scan = LaserScan()
+		scan.header.stamp = current_time - rospy.Duration(cycle) # rospycycle 
+		scan.header.frame_id = 'laser_frame'
+
+		scan.angle_min = 0.0
+		rpmcycle = cycle
+		scan.angle_max = (readInterval*(count-1))/rpmcycle * 2 * math.pi
+		scan.angle_increment = (scan.angle_max-scan.angle_min) / (count-1)  	
+
+		scan.time_increment =  readInterval
+		scan.scan_time = readInterval * count # cycle # rospycycle.to_sec()
+
+		scan.range_min = minimum_range
+		scan.range_max = maximum_range
+
+		zeroes = 0
+		scan.ranges=[]
+		# temp=[]
+		for x in range(len(raw_data)-(count*2)-headercodesize, len(raw_data)-headercodesize, 2):
+			low = ord(raw_data[x])
+			high = ord(raw_data[x+1])
+			value = ((high<<8)|low) / 100.0
+			scan.ranges.append(value)
+			# temp.append(value)
+
+		""" rotate if hideHeaderInMask """
+		# if hideHeaderInMask: 
+			# split = int((360-hideHeaderAngle)/360.0*count)
+			# scan.ranges = temp[split:]+temp[0:split]
+
+		""" blank frame masks W/RHR+ degrees start, stop """
+		i = 0
+		while i < len(masks):
+			for x in range(int(count*((masks[i])/360.0)), int(count*((masks[i+1])/360.0)) ):
+				scan.ranges[x] = 0
+			i += 2
+			
+		""" if blanking scans when turning """
+		if dropscan: 	
+			for i in range(len(scan.ranges)):
+				scan.ranges[i] = 0
+		dropscan = False
+		
+		""" publish scan """	
+		scan_pub.publish(scan)
+		
+		del raw_data[:] 
+
+
 
 # main
 
@@ -194,151 +367,5 @@ ser = usbdiscover.usbdiscover("<id::xaxxonopenlidar>")
 Server(XaxxonOpenLidarConfig, dynamicConfigCallback)
 client = dynamic_reconfigure.client.Client("lidarbroadcast", timeout=30)
 
-
-ser.write("y\n") # get version
-line = ""
-rospy.sleep(0.1)
-while ser.inWaiting() > 0:
-	line = ser.readline().strip()
-	print(line)
-	
-ser.write("r"+chr(rpm)+"\n")  
-ser.write("d"+chr(DIR)+"\n")  # set direction (1=CW RHR+ motor@bottom default, ROS default)
-ser.write("a\n") # start lidar
-
-# clear buffer
-ser.reset_input_buffer()
-
-raw_data = []
-lastscan = rospy.Time.now()
-headercodesize = 4
-current_time = 0
-dropscan = False
-scannum = 0
-
-while not rospy.is_shutdown() and ser.is_open:
-
-	# read data and dump into array, checking for header code 0xFF,0xFF,0xFF,0xFF
-	ch = ser.read(1)  
-	
-	# if timed out (5 seconds, defined in usbdiscover.py)
-	if len(ch) == 0:
-		rospy.logerr("no response from xaxxonlidar device")
-		break
-	
-	raw_data.append(ch)
-	
-	if turning:
-		dropscan = True
-			
-	if not ord(ch) == 0xFF:
-		continue
-	else:
-		ch = ser.read(1)
-		raw_data.append(ch)
-		if not ord(ch) == 0xFF:
-			continue
-		else: 
-			ch = ser.read(1)
-			raw_data.append(ch)
-			if not ord(ch) == 0xFF:
-				continue
-			else: 
-				ch = ser.read(1)
-				raw_data.append(ch)
-				if not ord(ch) == 0xFF:
-					continue
-
-	ser.write("h\n") # send host hearbeat (every <10 sec minimum)
-
-	# read count		
-	low = ord(ser.read(1))
-	high = ord(ser.read(1))
-	count = (high<<8)|low
-	
-	""" read cycle """
-	c1 = ord(ser.read(1))
-	c2 = ord(ser.read(1))
-	c3 = ord(ser.read(1))
-	c4 = ord(ser.read(1))
-	cycle = ((c4<<24)|(c3<<16)|(c2<<8)|c1)/1000000.0
-
-	""" host time """
-	current_time = rospy.Time.now() # - rospy.Duration(0.0) #0.015
-	rospycycle = current_time - lastscan
-	# cycle = rospycycle.to_sec()
-	lastscan = current_time	
-	
-	rospycount = (len(raw_data)-headercodesize)/2
-	
-	# startup debug info
-	if debugoutput:
-		if not count == 0:
-			print "cycle: "+str(cycle)
-			print "rospycycle: "+str(rospycycle.to_sec())
-			print "count: "+str(count)
-			print "scannum: "+str(scannum)
-		if not rospycount == count:
-			print "*** COUNT/DATA MISMATCH *** "+ str( rospycount-count )
-		print " "
-	
-		if scannum > 10:
-			debugoutput = False
-	
-	scannum += 1	
-	if scannum <= 3: # drop 1st few scans while lidar spins up
-		del raw_data[:]
-		continue
-	
-	scan = LaserScan()
-	scan.header.stamp = current_time - rospy.Duration(cycle) # rospycycle 
-	scan.header.frame_id = 'laser_frame'
-
-	scan.angle_min = 0.0
-	rpmcycle = cycle
-	scan.angle_max = (readInterval*(count-1))/rpmcycle * 2 * math.pi
-	scan.angle_increment = (scan.angle_max-scan.angle_min) / (count-1)  	
-
-	scan.time_increment =  readInterval
-	scan.scan_time = readInterval * count # cycle # rospycycle.to_sec()
-
-	scan.range_min = minimum_range
-	scan.range_max = maximum_range
-
-	zeroes = 0
-	scan.ranges=[]
-	# temp=[]
-	for x in range(len(raw_data)-(count*2)-headercodesize, len(raw_data)-headercodesize, 2):
-		low = ord(raw_data[x])
-		high = ord(raw_data[x+1])
-		value = ((high<<8)|low) / 100.0
-		scan.ranges.append(value)
-		# temp.append(value)
-
-	""" rotate if hideHeaderInMask """
-	# if hideHeaderInMask: 
-		# split = int((360-hideHeaderAngle)/360.0*count)
-		# scan.ranges = temp[split:]+temp[0:split]
-
-	""" blank frame masks W/RHR+ degrees start, stop """
-	i = 0
-	while i < len(masks):
-		for x in range(int(count*((masks[i])/360.0)), int(count*((masks[i+1])/360.0)) ):
-			scan.ranges[x] = 0
-		i += 2
-		
-	""" if blanking scans when turning """
-	if dropscan: 	
-		for i in range(len(scan.ranges)):
-			scan.ranges[i] = 0
-	dropscan = False
-	
-	""" publish scan """	
-	scan_pub.publish(scan)
-	
-	del raw_data[:] 
-
-	# if scannum % 10 == 0:
-		# msg = "scan #: "+str(scannum)
-		# rospy.loginfo(msg)
+readlidar(ser)
 
